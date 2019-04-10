96c1f0e42ef6ea09262247a11b152658c0f6578e
Pablo <pablopereznovoa@gmail.com>
2019-04-10 18:37:00 +0200

    'v4'

---
+++ build/precache-manifest.9a78c54753db5b57670dce6ba06eb133.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "16319b955b396f5606a3",
+    "url": "/static/js/main.3b65873f.chunk.js"
+  },
+  {
+    "revision": "6d9351d82e9c27a1b2a9",
+    "url": "/static/js/2.dee79f4b.chunk.js"
+  },
+  {
+    "revision": "16319b955b396f5606a3",
+    "url": "/static/css/main.6cf5f1b5.chunk.css"
+  },
+  {
+    "revision": "463e9eea7676f69c0de338bacaedac52",
+    "url": "/index.html"
+  }
+];
